f547746fdd855cf86ac57657a93eb5cae584ad16
Jarrett Pon <jpon@sfmbpjpon.local>
2018-06-04 16:36:11 -0700

    Testing with yarn (npm) link to import packages.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Fancy from 'react-fancy-component';
+import Graphdata from 'graph-data';
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +16,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Fancy />
+        <Graphdata graphdata='{"labels":["May","June","July","August","September","October","November","December","January","February","March","April","May","June","July"],"datasets":[{"label":["Google organic referrals"],"borderColor":["rgba(255, 0, 0, 1)"],"backgroundColor":["rgba(255, 255, 255, 0)"],"data":["21280940","26039230","30288282","28679481","24482326","24146618","24887769","29513074","28857876","27850754","32722568","30559119","30649938","32646344","28608123"]}]}'
+          graphtype="line"
+          caption="funtime"/>
       </div>
     );
   }
